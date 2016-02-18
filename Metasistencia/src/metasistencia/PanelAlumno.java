e6e3dc385160dfa70ff3983d32d26e6684980b3c
Kriossin <Key.Roxas@hotmail.com>
2016-02-18 19:30:09 +0100

    ignore

--- Metasistencia/src/metasistencia/PanelAlumno.java
+++ Metasistencia/src/metasistencia/PanelAlumno.java
@@ -1,5 +1,34 @@
 package metasistencia;
 
-public class PanelAlumno {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import model.Alumno;
+
+public class PanelAlumno extends JPanel implements ActionListener{
+
+	FramePrincipal framePrincipal;
+
+	static Alumno alumnoSeleccionado;
+	Font fuente = new Font("Century Gothic", Font.BOLD, 20);
+	
+	public PanelAlumno(JFrame framePrincipal){
+	
+		//Inicializar
+		
+		this.setLayout(null);
+		
+		this.framePrincipal = (FramePrincipal) framePrincipal;
+}
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
